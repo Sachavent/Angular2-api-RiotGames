bb55c47194203d65329be412b01500fb351d20ec
Sachavent <s.ventribout@gmail.com>
2017-02-21 14:49:07 +0100

    try

--- server.js
+++ server.js
@@ -11,15 +11,22 @@ app.use(express.static(__dirname + '/dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(function (req, res, next) {
+
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   next();
 });
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function (req, res) {
+app.get('/*', function (req, res, next) {
+    next();
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 // Start the app by listening on the default
