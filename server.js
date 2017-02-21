2dbc268787f0d0a4813adbf493ef9d8bfcfd070b
Sachavent <s.ventribout@gmail.com>
2017-02-21 14:10:32 +0100

    test

--- server.js
+++ server.js
@@ -6,6 +6,25 @@ const path = require('path');
 /**
  * Resolving the problem of "No Access allow origin"
  */
+var sess = {
+  secret: 'keyboard cat',
+  proxy: true,
+  resave: false,
+  saveUninitialized: false
+}
+
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1) // trust first proxy
+ // sess.cookie.secure = true // serve secure cookies
+}
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.use(session(sess))
 app.use(function (req, res, next) {
 
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -31,4 +50,4 @@ app.get('/*', function (req, res) {
 });
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
