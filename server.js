eff92bcfde47d64eb63fb47f3dd624a15db838d2
Sachavent <s.ventribout@gmail.com>
2017-02-21 17:23:42 +0100

    le probleme vient de la requete....

--- server.js
+++ server.js
@@ -1,29 +1,7 @@
 // server.js
 const express = require('express');
 const app = express();
-const path = require('path');
-var bodyParser = require('body-parser');
 
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-if (app.get('env') === 'production') {
-  app.set('trust proxy', 1) // trust first proxy
-}
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(function (req, res, next) {
-
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  next();
-});
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
