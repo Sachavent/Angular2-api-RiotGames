e19f0439c96172b07a1a5019b960c5701b9da432
Sachavent <s.ventribout@gmail.com>
2017-02-21 17:26:01 +0100

    small update

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
