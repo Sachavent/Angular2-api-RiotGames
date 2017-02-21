edeabcb07238e80e855d364e74d581ed678b9788
Sachavent <s.ventribout@gmail.com>
2017-02-21 14:39:54 +0100

    try

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 // Run the app by serving the static files
@@ -10,8 +9,7 @@ var bodyParser = require('body-parser');
 app.use(express.static(__dirname + '/dist'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
