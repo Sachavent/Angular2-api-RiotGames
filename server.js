d7931835a8e48bf693312e39bda7f224ead30c9e
Sachavent <s.ventribout@gmail.com>
2017-02-21 14:02:28 +0100

    try it

--- server.js
+++ server.js
@@ -6,12 +6,19 @@ const path = require('path');
 /**
  * Resolving the problem of "No Access allow origin"
  */
-/*app.get('/*', function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://lol-data-angular2.herokuapp.com/');
-    res.header('Access-Control-Allow-Credentials', true);
-    next();
-}
-);*/
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  next();
+});
+
 
 // Run the app by serving the static files
 // in the dist directory
@@ -20,13 +27,7 @@ app.use(express.static(__dirname + '/dist'));
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function (req, res) {
-    res.header('Access-Control-Allow-Origin', 'https://lol-data-angular2.herokuapp.com');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
-    res.header('Access-Control-Allow-Credentials', true);
-
     res.sendFile(path.join(__dirname + '/dist/index.html'));
-
 });
 // Start the app by listening on the default
 // Heroku port
