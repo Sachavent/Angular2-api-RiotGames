3627103ee2074bc02c58519e0325c9bfe58b1f80
Sachavent <s.ventribout@gmail.com>
2017-02-21 13:51:56 +0100

    try fixing

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const path = require('path');
 /**
  * Resolving the problem of "No Access allow origin"
  */
-app.get('/*',function (req, res, next) {
+/*app.get('/*', function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'https://lol-data-angular2.herokuapp.com/');
     res.header('Access-Control-Allow-Credentials', true);
     next();
 }
-);
+);*/
 
 // Run the app by serving the static files
 // in the dist directory
@@ -19,8 +19,15 @@ app.use(express.static(__dirname + '/dist'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function (req, res) {
+app.get('/*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+    res.header('Access-Control-Allow-Credentials', true);
+
     res.sendFile(path.join(__dirname + '/dist/index.html'));
+    next();
+
 });
 // Start the app by listening on the default
 // Heroku port
