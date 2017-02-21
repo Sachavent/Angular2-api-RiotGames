385fe6260bedbcc00e9a7286360b2f17aa14b6e0
Sachavent <s.ventribout@gmail.com>
2017-02-21 14:14:13 +0100

    test

--- server.js
+++ server.js
@@ -6,26 +6,8 @@ const path = require('path');
 /**
  * Resolving the problem of "No Access allow origin"
  */
-var sess = {
-  secret: 'keyboard cat',
-  proxy: true,
-  resave: false,
-  saveUninitialized: false
-}
 
-if (app.get('env') === 'production') {
-  app.set('trust proxy', 1) // trust first proxy
- // sess.cookie.secure = true // serve secure cookies
-}
-
-var bodyParser = require('body-parser')
-app.use(bodyParser.json());       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
-
-app.use(session(sess))
-app.use(function (req, res, next) {
+app.all('/',function (req, res, next) {
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Credentials', true);
@@ -34,7 +16,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
 
