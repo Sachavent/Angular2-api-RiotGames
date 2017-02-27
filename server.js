2cdbe6eda818f544fe1f22810ffeade6d935a889
Sachavent <s.ventribout@gmail.com>
2017-02-27 11:37:18 +0100

    trying to add body parser

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
