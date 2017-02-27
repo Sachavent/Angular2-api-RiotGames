340bf5cd958e0a6588a3d54b42eb0a1a512d561f
Sachavent <s.ventribout@gmail.com>
2017-02-27 11:12:20 +0100

    test readfile

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 // Run the app by serving the static files
 // in the dist directory
@@ -10,6 +11,11 @@ app.use(express.static(__dirname + '/dist'));
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function (req, res) {
+
+    fs.readFile(__dirname + '/dist/index.html', (err, data) => {
+        if (err) throw err;
+        console.log(data);
+    });
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 // Start the app by listening on the default
