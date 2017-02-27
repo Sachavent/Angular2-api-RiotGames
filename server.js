9d354d5420d132392e840358feb80fe76e193f72
Sachavent <s.ventribout@gmail.com>
2017-02-27 11:26:22 +0100

    test add lowercase

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const fs = require('fs');
 
 // Run the app by serving the static files
 // in the dist directory
@@ -11,11 +10,6 @@ app.use(express.static(__dirname + '/dist'));
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function (req, res) {
-
-    fs.readFile(__dirname + '/dist/assets/tier_icon', (err, data) => {
-        if (err) throw err;
-        console.log(data);
-    });
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 // Start the app by listening on the default
